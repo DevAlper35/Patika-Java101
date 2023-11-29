97a8bc3298077b78ff6402ea351c37ea8fcfe3c5
DevAlper35 <devcalisma@gmail.com>
2023-11-29 22:00:59 +0300

    Java101

---
+++ Odevler/src/NotHesaplamaUygulamasi.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class NotHesaplamaUygulamasi {
+    public static void main(String[] args) {
+
+        Scanner matematik=new Scanner(System.in);
+        System.out.print("Matematik notunu giriniz: ");
+        int a = matematik.nextInt();
+
+        Scanner fizik=new Scanner(System.in);
+        System.out.print("Fizik notunu giriniz: ");
+        int b = fizik.nextInt();
+
+        Scanner kimya=new Scanner(System.in);
+        System.out.print("Kimya notunu giriniz: ");
+        int c = kimya.nextInt();
+
+        Scanner turkce=new Scanner(System.in);
+        System.out.print("Türkçe notunu giriniz: ");
+        int d = turkce.nextInt();
+
+        Scanner tarih=new Scanner(System.in);
+        System.out.print("Tarih notunu giriniz: ");
+        int e = tarih.nextInt();
+
+        Scanner muzik=new Scanner(System.in);
+        System.out.print("Müzik notunu giriniz: ");
+        int f = muzik.nextInt();
+
+        double ortalama=((a+b+c+d+e+f)/6);
+        System.out.println("ortalamanız:" + ortalama);
+
+        String sonuc = ortalama >= 60 ? "Sınıfı geçti" : "Sınıfta kaldı";
+        System.out.print(sonuc);
+
+    }
+}
