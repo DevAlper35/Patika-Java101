3a30355c76a36229e552d65cc9fcaab144e68bef
DevAlper35 <devcalisma@gmail.com>
2023-11-30 21:18:16 +0300

    Taksi metre Programi - Ödev

---
+++ Odevler/src/TaksimetreProgrami.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class TaksimetreProgrami {
+    public static void main (String[]args){
+        Scanner input = new Scanner(System.in);
+        int acilis = 10;
+        System.out.print("Kilometreyi giriniz: ");
+        int km = input.nextInt();
+        double tutarKm = (km * 2.2) + 10;
+        double toplamTutar = (tutarKm >=20)? tutarKm : 20;
+        System.out.println("Toplam tutar " + toplamTutar + "TL");
+    }
+
+
+
+
+}
