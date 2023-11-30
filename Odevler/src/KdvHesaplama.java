a41c8b25649d19a94092d37a1f2d1d51dee868ed
DevAlper35 <152246898+DevAlper35@users.noreply.github.com>
2023-11-30 21:37:11 +0300

    Kdv Hesaplama - Ödev

--- Odevler/src/KdvHesaplama.java
+++ Odevler/src/KdvHesaplama.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 public class KdvHesaplama {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
         System.out.print("Tutar girin: ");
         double tutar = input.nextInt();
         double kdvlifiyat = tutar >= 1000 ? (tutar * 0.08)+tutar : (tutar * 0.18)+tutar;
