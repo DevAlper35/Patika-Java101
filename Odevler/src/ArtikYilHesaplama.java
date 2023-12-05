24a1c3a7730e05c3070c21508a3b0eec41e2cf77
DevAlper35 <devcalisma@gmail.com>
2023-12-05 23:59:02 +0300

    Artık Yıl Hesaplama - Ödev

---
+++ Odevler/src/ArtikYilHesaplama.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class ArtikYilHesaplama {
+    public static void main(String[] args) {
+        int year;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Yıl Giriniz : ");
+        year = input.nextInt();
+
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                System.out.println(year + " Yılı artık bir yıldır.");
+            } else {
+                System.out.println(year + " Yılı artık bir yıl değildir.");
+            }
+
+
+        } else if (year % 4 == 0) {
+            System.out.println(year + " Yılı artık bir yıldır.");
+
+        } else {
+            System.out.println(year + " Yılı artık bir yıl değildir.");
+        }
+    }
+}
