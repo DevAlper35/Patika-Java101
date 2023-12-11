b08aaa9a7c2408601e90142a1688dec9a17bd19c
DevAlper35 <152246898+DevAlper35@users.noreply.github.com>
2023-12-12 00:00:52 +0300

    Harmonik Sayı Bulma

---
+++ Odevler/src/HarmonikSayiBulma.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class HarmonikSayiBulma {
+    public static void main(String[] args) {
+        System.out.println("Harmonik Sayilari Bulan Program");
+        Scanner input = new Scanner(System.in);
+
+        int number;
+        double result=0.0;
+        System.out.print("Lutfen bir sayi giriniz: ");
+        number = input.nextInt();
+
+        for(int i=1; i<=number; i++)
+        {
+            result += (1.0/i);
+        }
+
+        System.out.println("Sonuç: "+ result);
+    }
+}
