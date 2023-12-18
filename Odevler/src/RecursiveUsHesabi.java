7111482cf80ce155f3bbfbb6e9768a2fdf3a9cf7
Alper MUDURLU <152246898+DevAlper35@users.noreply.github.com>
2023-12-18 08:38:13 +0300

    RecursiveUsHesabi.java

--- Odevler/src/RecursiveUsHesabi.java
+++ Odevler/src/RecursiveUsHesabi.java
@@ -1,17 +1,11 @@
 import java.util.Scanner;
 public class RecursiveUsHesabi {
     public static void main(String[] args) {
-        /*
-            PatikaDev Java101 - Advanced Calculator
-            A program that uses the "Recursive" method for exponentiation with base and exponent values taken from the user.
-            Note: There is no exception checking.
-         */
-
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.print("Taban sayısınız girin: ");
-        int base = scanner.nextInt();
+        int base = input.nextInt();
         System.out.print("Üs sayısını girin: ");
-        int exponent = scanner.nextInt();
+        int exponent = input.nextInt();
 
         System.out.println("Sonuç= " + exponentiation(base, exponent));
     }
